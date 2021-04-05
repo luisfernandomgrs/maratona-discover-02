7a775b83ecdd07379a240b36e1c353cad8ce1f8c
Luis Fernando M <luisfernandomgrs@gmail.com>
2021-04-05 01:20:23 -0300

    check validation to free hours

--- src/controllers/DashboardController.js
+++ src/controllers/DashboardController.js
@@ -33,7 +33,7 @@ module.exports = {
         // horas livres, considerando trabalhos encerrados..
         // const freeHours = profile["hours-per-day"] - jobTotalHours;
         profile["free-hours"] = Number(profile["hours-per-day"] - jobTotalHours).toFixed(2);
-        profile["free-hours-msg"] = (profile["free-hours"] <= 0) ? "Você não possui horas livres" : `Você tem ${profile["free-hours"]} horas livres no seu dia`;
+        profile["free-hours-msg"] = (profile["free-hours"] <= 0) ? "Você não possui horas livres" : `Você tem ${Number(profile["free-hours"]).toFixed()} horas livres no seu dia`;
 
         // return res.render("index", {jobs: updatedJobs, profile: Profile.data});
         // return res.render("index", {jobs: updatedJobs, profile: profile, statusCount: statusCount, freeHours: freeHours});
